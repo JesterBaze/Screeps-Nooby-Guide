43af2ed11ca38eac5bb4e93c4f95da9451e991c8
JesterBaze <kdskater01@aol.com>
2016-07-06 11:53:36 -0400

    Move idle harvesters
    Create a rally point called 'FlagHarvestRally' and move harvesters there when idle / resources are full.

--- role.harvester.js
+++ role.harvester.js
@@ -1,7 +1,7 @@
 module.exports = {
     // a function to run the logic for this role
     run: function(creep) {
-        // if creep is bringing energy to a structure but has no energy left
+        // if creep is bringing energy to the spawn or an extension but has no energy left
         if (creep.memory.working == true && creep.carry.energy == 0) {
             // switch state
             creep.memory.working = false;
@@ -12,18 +12,17 @@ module.exports = {
             creep.memory.working = true;
         }
 
-        // if creep is supposed to transfer energy to a structure
+        // if creep is supposed to transfer energy to the spawn or an extension
         if (creep.memory.working == true) {
-            // find closest spawn, extension or tower which is not full
+            // find closest spawn or extension which is not full
             var structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
                 // the second argument for findClosestByPath is an object which takes
                 // a property called filter which can be a function
                 // we use the arrow operator to define it
-                filter: (s) => (s.structureType == STRUCTURE_SPAWN
-                             || s.structureType == STRUCTURE_EXTENSION
-                             || s.structureType == STRUCTURE_TOWER)
-                             && s.energy < s.energyCapacity
-            });
+                filter: (s) => (s.structureType == STRUCTURE_TOWER
+                || s.structureType == STRUCTURE_EXTENSION
+                || s.structureType == STRUCTURE_SPAWN)
+                && s.energy < s.energyCapacity});
 
             // if we found one
             if (structure != undefined) {
@@ -39,9 +38,13 @@ module.exports = {
             // find closest source
             var source = creep.pos.findClosestByPath(FIND_SOURCES);
             // try to harvest energy, if the source is not in range
-            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+            if (creep.harvest(source) == ERR_NOT_IN_RANGE || ERR_NOT_ENOUGH_ENERGY) {
                 // move towards the source
                 creep.moveTo(source);
+                //If Resources are occupied, move to rally point.
+                if (creep.harvest(source) == ERR_NO_PATH || ERR_FULL){
+                    creep.moveTo(Game.flags.FlagHarvestRally);
+                }
             }
         }
     }
